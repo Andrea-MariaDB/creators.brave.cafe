a83ef3144b22886cbb518b239291fa27105e433f
danut.andres <goez482@gmail.com>
2021-05-11 12:24:49 +0200

    Added an .js file ,changed some styles and added about us page

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+var urlArray = [
+    "https://brave.cafe/",
+    "https://pbanks.net/",
+    "https://www.guilhermepantoja.com/",
+    "https://carolpelu.com/",
+    "https://recipeasly.org/",
+    "https://ashkankamyab.com/",
+    "https://cubecube.org/",
+    "https://twitter.com/andrei_pestee",
+    "https://3dprintguides.com/",
+    "https://limbo.center/"
+];
+
+
+function randomUrl() {
+    var randomNumber = Math.floor(Math.random() * urlArray.length);
+    var newUrl = urlArray[randomNumber];
+    window.location.href = newUrl;
+}
